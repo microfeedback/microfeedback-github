1d62b66345e585c74cbf35dda051aa79b8109a80
Steven Loria <sloria1@gmail.com>
2017-05-10 22:10:46 -0400

    Format input as Markdown
    Includes
    
    - Message
    - Headers
    - Browser info
    - OS info
    - Extra info passed from client

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const assert = require('assert');
 
 const parseGH = require('parse-github-repo-url');
+const parseUserAgent = require('ua-parser-js');
+const truncate = require('truncate');
+const table = require('markdown-table');
 const axios = require('axios');
 const { createError } = require('micro');
 const wishes = require('micro-wishes');
@@ -14,8 +17,57 @@ assert(GH_TOKEN, 'GH_TOKEN not set');
 const [username, repoName] = parseGH(GH_REPO);
 const GH_URL = `https://api.github.com/repos/${username}/${repoName}/issues`;
 
-const GitHubBackend = async ({ body }) => {
-  const title = '[wishes] TODO';
+const HEADER_WHITELIST = ['user-agent', 'origin', 'referer'];
+
+const makeTable = (headers, entries, sort = true) => {
+  const ret = [headers];
+  const orderedEntries = sort ? entries.sort((a, b) => a[0] > b[0]) : entries;
+  orderedEntries.forEach((each) => {
+    ret.push(each);
+  });
+  return table(ret);
+};
+
+const makeIssue = ({ body, extra }, req) => {
+  let suffix = '';
+  if (req && req.headers.referer) {
+    suffix = ` on ${req.headers.referer}`;
+  }
+  const title = `[wishes] New feedback${suffix}: "${truncate(body, 25)}"`;
+  let fullBody = `:bulb: New feedback was posted${suffix}.\n\n## Message\n\n${body}\n\n-----\n`;
+  // Format headers as table
+  if (req && req.headers) {
+    const entries = Object.entries(req.headers).filter(
+      e => HEADER_WHITELIST.indexOf(e[0]) >= 0
+    );
+    const headerTable = makeTable(['Header', 'Value'], entries);
+    fullBody += `\n### Headers\n\n${headerTable}\n`;
+  }
+  // Format user agent info as table
+  if (req && req.headers && req.headers['user-agent']) {
+    const userAgent = parseUserAgent(req.headers['user-agent']);
+    const browserEntries = Object.entries(userAgent.browser).filter(e => e[1]);
+    if (browserEntries.length) {
+      const browserTable = makeTable(['Key', 'Value'], browserEntries, false);
+      fullBody += `\n### Browser\n\n${browserTable}\n`;
+    }
+
+    const osEntries = Object.entries(userAgent.os).filter(e => e[1]);
+    if (osEntries.length) {
+      const osTable = makeTable(['Key', 'Value'], osEntries, false);
+      fullBody += `\n### Operating System\n\n${osTable}\n`;
+    }
+  }
+  // Format extra information as table
+  if (extra) {
+    const extraTable = makeTable(['Key', 'Value'], Object.entries(extra));
+    fullBody += `\n### Extra information\n\n${extraTable}`;
+  }
+  fullBody += `\nReported via *[${pkg.name}](${pkg.repository}) v${pkg.version}*.`;
+  return { title, body: fullBody };
+};
+
+const GitHubBackend = async (input, req) => {
   try {
     const { data } = await axios({
       method: 'POST',
@@ -23,10 +75,7 @@ const GitHubBackend = async ({ body }) => {
       params: {
         access_token: GH_TOKEN,
       },
-      data: {
-        title,
-        body,
-      },
+      data: makeIssue(input, req),
     });
     return data;
   } catch (err) {
@@ -39,4 +88,4 @@ module.exports = wishes(GitHubBackend, {
   name: 'github',
   version: pkg.version,
 });
-Object.assign(module.exports, { GitHubBackend, GH_URL });
+Object.assign(module.exports, { GitHubBackend, GH_URL, makeIssue });
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const MockAdapter = require('axios-mock-adapter');
 
-const { GitHubBackend, GH_URL } = require('./');
+const { GitHubBackend, GH_URL, makeIssue } = require('./');
 
 test('POST: posts to GitHub API', async (t) => {
   const mock = new MockAdapter(axios);
@@ -14,4 +14,55 @@ test('POST: posts to GitHub API', async (t) => {
   mock.onPost(GH_URL).reply(201, mockResponse);
   const result = await GitHubBackend({ name: 'Steve', body: 'test' });
   t.deepEqual(result, mockResponse);
+});
+
+test('makeIssue returns title and body', (t) => {
+  const input = { body: 'foo' };
+  const result = makeIssue(input);
+  t.truthy(result.title);
+  t.truthy(result.body);
+});
+
+test('makeIssue returns formatted title', (t) => {
+  const req = { headers: { 'x-foo': 'bar', referer: 'https://test.test/test?test' } };
+  const input = { body: 'foo' };
+  const { title } = makeIssue(input, req);
+  t.is(title, `[wishes] New feedback on ${req.headers.referer}: "foo"`);
+});
+
+test('makeIssue returns headers in body', (t) => {
+  const req = { headers: { 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Ubuntu/11.10 Chromium/15.0.874.106 Chrome/15.0.874.106 Safari/535.2' } };
+  const input = { body: 'foo' };
+  const { body } = makeIssue(input, req);
+  t.regex(body, /user-agent/);
+  t.regex(body, /Mozilla\/5.0/);
+});
+
+test('makeIssue returns extra info', (t) => {
+  const input = {
+    body: 'test',
+    extra: {
+      b: 'bar',
+      a: 'foo',
+      c: 'baz',
+    },
+  };
+  const { body } = makeIssue(input);
+  t.regex(body, /xtra/);
+});
+
+test('makeIssue returns browser info', (t) => {
+  const req = { headers: { 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Ubuntu/11.10 Chromium/15.0.874.106 Chrome/15.0.874.106 Safari/535.2' } };
+  const input = { body: 'test' };
+  const { body } = makeIssue(input, req);
+  t.regex(body, /### Browser/);
+  t.regex(body, /Chromium/);
+});
+
+test('makeIssue returns OS info', (t) => {
+  const req = { headers: { 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Ubuntu/11.10 Chromium/15.0.874.106 Chrome/15.0.874.106 Safari/535.2' } };
+  const input = { body: 'test' };
+  const { body } = makeIssue(input, req);
+  t.regex(body, /### Operating System/);
+  t.regex(body, /Ubuntu/);
 });
